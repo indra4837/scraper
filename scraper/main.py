e3410625b0415df4635f0d7ad8d5337789f17ea8
indra4837 <indra4837@gmail.com>
2021-11-19 19:13:01 +0100

    refactored services and added skaffold config file

--- scraper/main.py
+++ scraper/main.py
@@ -12,7 +12,9 @@ CHECK_TEXT = "Sorry. No tickets available at the moment."
 HEADERS_BS4 = {
     "User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"
 }
-TELEBOT_URL = config("TELEBOT_URL")
+
+# TELEBOT_URL = config("TELEBOT_URL")
+TELEBOT_URL = "http://telebot-srv:3000"
 
 
 def check_BVB():
